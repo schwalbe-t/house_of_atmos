0c8c8e4fe019448911d8b0d141d563da8c253329
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-10-11 18:39:22 +0300

    Small cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ struct CubeShader: engine::rendering::Shader<CubeVertex, CubeShader> {
 int main() {
     engine::init("House of Atmos", 800, 800, 60);
     auto main_buffer = engine::rendering::Surface(800, 800);
-    auto sub_buffer = engine::rendering::Surface(800, 800);
+    auto sub_buffer = engine::rendering::Surface(200, 200);
     auto square = engine::rendering::Mesh<CubeVertex>();
     square.add_vertex({ Vec<3>(-1.0, -1.0,  1.0), Vec<3>(1.0, 0.0, 0.0) });
     square.add_vertex({ Vec<3>( 1.0, -1.0,  1.0), Vec<3>(0.0, 1.0, 0.0) });
@@ -57,15 +57,14 @@ int main() {
     square.add_element(6, 7, 3);
     auto shader = CubeShader();
     shader.projection = Mat<4>::perspective(PI / 2.0, sub_buffer.width / sub_buffer.height, 0.1, 1000.0);
-    //shader.projection = Mat<4>::orthographic(-3, 3, 3, -3, 0.1, 1000.0);
-    shader.view = Mat<4>::look_at(Vec<3>(3, 0, 3), Vec<3>(0, 0, 0), Vec<3>(0, 1, 0));
+    shader.view = Mat<4>::look_at(Vec<3>(0, 3, 3), Vec<3>(0, 0, 0), Vec<3>(0, 1, 0));
     double rot_angle = 0.0;
     while(engine::is_running()) {
         rot_angle += GetFrameTime();
         main_buffer.clear();
         sub_buffer.clear();
-        shader.model = Mat<4>::rotate_y(rot_angle)
-            * Mat<4>::rotate_z(rot_angle);
+        shader.model = Mat<4>::rotate_y(rot_angle);
+            //* Mat<4>::rotate_z(rot_angle);
         sub_buffer.draw_mesh(square, shader);
         main_buffer.blit_buffer(sub_buffer, 0, 0, 800, 800);
         engine::display_buffer(&main_buffer);
