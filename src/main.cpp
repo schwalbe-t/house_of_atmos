e8ab2cd86dca53433ef91cf6009534de04994b67
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-11-21 21:38:15 +0200

    Move the rendering engine into its own repository

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 
-#include "engine/engine.hpp"
-#include "engine/resources.hpp"
+#include <druck/window.hpp>
+#include <druck/resources.hpp>
 
-namespace engine = houseofatmos::engine;
-namespace rendering = houseofatmos::engine::rendering;
-namespace resources = houseofatmos::engine::resources;
-namespace animation = houseofatmos::engine::animation;
-using namespace houseofatmos::engine::math;
+namespace rendering = druck::rendering;
+namespace resources = druck::resources;
+namespace animation = druck::animation;
+using namespace druck::math;
 
 
 // struct ModelShader: engine::rendering::Shader<engine::resources::ModelVertex, ModelShader> {
@@ -58,8 +57,8 @@ struct RiggedModelShader: rendering::Shader<resources::RiggedModelVertex, Rigged
 };
 
 int main() {
-    engine::init("House of Atmos", 1200, 800, 60);
-    auto model = engine::resources::read_gltf_model("res/player.gltf");
+    druck::init("House of Atmos", 1200, 800, 60);
+    auto model = resources::read_gltf_model("res/player.gltf");
     animation::Animation floss = model.animations["floss"];
     auto main_buffer = rendering::Surface(1200, 800);
     auto sub_buffer = rendering::Surface(1200, 800);
@@ -68,16 +67,16 @@ int main() {
     model_shader.view = Mat<4>::look_at(Vec<3>(0, 5, 10), Vec<3>(0, 5, 0), Vec<3>(0, 1, 0));
     model_shader.bones = &model.bones;
     double anim_timer = 0.0;
-    while(engine::is_running()) {
+    while(druck::is_running()) {
         anim_timer = fmod(anim_timer + GetFrameTime(), floss.length);
         floss.compute_transforms(model.bones, model.root_bone_i, anim_timer);
         main_buffer.clear();
         sub_buffer.clear();
         model.draw(sub_buffer, model_shader);
         main_buffer.blit_buffer(sub_buffer, 0, 0, 1200, 800);
-        engine::display_buffer(&main_buffer);
+        druck::display_buffer(&main_buffer);
     }
-    engine::stop();
+    druck::stop();
 }
 
 
